f0f0de1da23faf69fdb083d0b3901f2ff0257d1d
Łukasz Solik <lukasz.s100@wp.pl>
2026-05-14 11:13:13 +0200

    add summary component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { Heading } from './main-content/heading/heading';
+import { Summary } from './main-content/summary/summary';
 
 @Component({
   selector: 'app-root',
-  imports: [MatSidenavModule, Heading],
+  imports: [MatSidenavModule, Heading, Summary],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
