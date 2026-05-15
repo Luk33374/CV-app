6d2cf5b5c8248258821b847c5854e33a2c314eb0
Łukasz Solik <lukasz.s100@wp.pl>
2026-05-15 12:13:10 +0200

    Add experience

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { Heading } from './main-content/heading/heading';
 import { Expander } from './main-content/expander/expander';
+import { Experience } from './main-content/experience/experience';
 
 @Component({
   selector: 'app-root',
-  imports: [MatSidenavModule, Heading, Expander],
+  imports: [MatSidenavModule, Heading, Expander, Experience],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
---
+++ src/app/main-content/experience/experience.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Expander } from '../expander/expander';
+
+@Component({
+  selector: 'app-experience',
+  imports: [Expander],
+  templateUrl: './experience.html',
+  styleUrl: './experience.scss',
+})
+export class Experience {}
