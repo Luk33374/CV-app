8e015db0a7c86350513588261c6a303d2b76091a
Łukasz Solik <lukasz.s100@wp.pl>
2026-05-15 13:57:16 +0200

    Change time formats

--- src/app/main-content/experience/experience.ts
+++ src/app/main-content/experience/experience.ts
@@ -9,8 +9,8 @@ import { ExpanderWithList } from '../expander-with-list/expander-with-list';
 })
 export class Experience {
   protected experienceItems = [
-    'Today – 2023: Angular Developer at Digital Technology Poland (DTP Ltd.), working on a project for Rittal. I was primarily responsible for developing and maintaining the front end of web applications using Angular and related technologies. The project focused on delivering high-quality software solutions for clients in the AI sector, particularly tools for calculating heat dissipation in AI data centers.',
-    '2023–2022: Frontend Developer at Comernal Software. I was responsible for developing and maintaining an application for organizing the production of electrical cabinets.',
-    '2021–2022: Intern at Eplan. Gained experience in agile methodologies and worked in quality assurance, contributing to test automation using Protractor and Cypress.',
+    '2023 - Present: Angular Developer at Digital Technology Poland (DTP Ltd.), working on a project for Rittal. I was primarily responsible for developing and maintaining the front end of web applications using Angular and related technologies. The project focused on delivering high-quality software solutions for clients in the AI sector, particularly tools for calculating heat dissipation in AI data centers.',
+    '2022–2023: Frontend Developer at Comernal Software. I was responsible for developing and maintaining an application for organizing the production of electrical cabinets.',
+    '2022–2021: Intern at Eplan. Gained experience in agile methodologies and worked in quality assurance, contributing to test automation using Protractor and Cypress.',
   ];
 }
