d9cc80f021506594772ac31633a4445e2ecc8a4b
Łukasz Solik <lukasz.s100@wp.pl>
2026-05-15 12:22:03 +0200

    Add list of items to component

--- src/app/main-content/experience/experience.ts
+++ src/app/main-content/experience/experience.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { Expander } from '../expander/expander';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-experience',
-  imports: [Expander],
+  imports: [Expander, MatListModule],
   templateUrl: './experience.html',
   styleUrl: './experience.scss',
 })
-export class Experience {}
+export class Experience {
+  protected experienceItems = ['Item 1', 'Item 2', 'Item 3'];
+}
