07d51d8f7a3d2fd9d8110ce90af605c9a966ff12
Łukasz Solik <lukasz.s100@wp.pl>
2026-05-15 13:34:41 +0200

    Add experience items

--- src/app/main-content/experience/experience.ts
+++ src/app/main-content/experience/experience.ts
@@ -11,8 +11,8 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class Experience {
   protected experienceItems = [
-    'Today - 2023: Angular Developer in Digital Technology Poland, DTP Ltd. I was mostly responsible for developing and maintaining the front-end of web applications using Angular and related technologies. The project which I was involved in focused on delivering high-quality software solutions to clients from AI sectors. The project was mostly focused on solutions for calculating heat dissipation in AI data centers.',
-    'Item 2',
-    'Item 3',
+    'Today – 2023: Angular Developer at Digital Technology Poland (DTP Ltd.), working on a project for Rittal. I was primarily responsible for developing and maintaining the front end of web applications using Angular and related technologies. The project focused on delivering high-quality software solutions for clients in the AI sector, particularly tools for calculating heat dissipation in AI data centers.',
+    '2023–2022: Frontend Developer at Comernal Software. I was responsible for developing and maintaining an application for organizing the production of electrical cabinets.',
+    '2021–2022: Intern at Eplan. Gained experience in agile methodologies and worked in quality assurance, contributing to test automation using Protractor and Cypress.',
   ];
 }
