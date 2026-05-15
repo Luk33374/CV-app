791d6a6c62fda8fd9a17b6d13c671f9fdd692f56
Łukasz Solik <lukasz.s100@wp.pl>
2026-05-15 12:03:44 +0200

    Remove experience

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,10 @@ import { Component, signal } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { Heading } from './main-content/heading/heading';
 import { Summary } from './main-content/summary/summary';
-import { Experience } from './main-content/experience/experience';
 
 @Component({
   selector: 'app-root',
-  imports: [MatSidenavModule, Heading, Summary, Experience],
+  imports: [MatSidenavModule, Heading, Summary],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
