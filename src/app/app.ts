052ffb090ca3e97465002700fbfbf93df21708ea
Łukasz Solik <lukasz.s100@wp.pl>
2026-05-15 12:02:27 +0200

    CREATE: experience component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { Heading } from './main-content/heading/heading';
 import { Summary } from './main-content/summary/summary';
+import { Experience } from './main-content/experience/experience';
 
 @Component({
   selector: 'app-root',
-  imports: [MatSidenavModule, Heading, Summary],
+  imports: [MatSidenavModule, Heading, Summary, Experience],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
