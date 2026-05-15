e1755cbb3e1a2745cb337937aaad9739483ba0ff
Łukasz Solik <lukasz.s100@wp.pl>
2026-05-15 13:54:40 +0200

    Add education info

--- src/app/main-content/education/education.ts
+++ src/app/main-content/education/education.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { ExpanderWithList } from '../expander-with-list/expander-with-list';
 
 @Component({
   selector: 'app-education',
-  imports: [],
+  imports: [ExpanderWithList],
   templateUrl: './education.html',
   styleUrl: './education.scss',
 })
-export class Education {}
+export class Education {
+  protected educationItems = [
+    '2019 - 2023: WSB Academy in Cieszyn, studying Computer Science with specialization in Mobile applications',
+  ];
+}
