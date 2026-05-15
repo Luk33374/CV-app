bf099a565c2f9258cb2d44a9e6373138168d987f
Łukasz Solik <lukasz.s100@wp.pl>
2026-05-15 14:45:11 +0200

    Add courses

--- src/app/drawer-content/courses/courses.ts
+++ src/app/drawer-content/courses/courses.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Expander } from '../../main-content/expander/expander';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [Expander],
   templateUrl: './courses.html',
   styleUrl: './courses.scss',
 })
