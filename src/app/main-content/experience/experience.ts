bb05679504a4ec6a7a9c1532a8b938c8825c2564
Łukasz Solik <lukasz.s100@wp.pl>
2026-05-15 13:15:33 +0200

    Add unordered icon and fix text wrapping

--- src/app/main-content/experience/experience.ts
+++ src/app/main-content/experience/experience.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { Expander } from '../expander/expander';
 import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-experience',
-  imports: [Expander, MatListModule],
+  imports: [Expander, MatListModule, MatIconModule],
   templateUrl: './experience.html',
   styleUrl: './experience.scss',
 })
 export class Experience {
-  protected experienceItems = ['Item 1', 'Item 2', 'Item 3'];
+  protected experienceItems = [
+    'Today - 2023: Angular Developer in Digital Technology Poland, DTP Ltd. I was mostly responsible for developing and maintaining the front-end of web applications using Angular and related technologies. The project which I was involved in focused on delivering high-quality software solutions to clients from AI sectors. The project was mostly focused on solutions for calculating heat dissipation in AI data centers.',
+    'Item 2',
+    'Item 3',
+  ];
 }
