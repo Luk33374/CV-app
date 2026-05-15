f4d5cd99d768841e3b8e3fe318d0824aec958aa6
Łukasz Solik <lukasz.s100@wp.pl>
2026-05-15 13:41:40 +0200

    CREATE: education component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { Heading } from './main-content/heading/heading';
 import { Expander } from './main-content/expander/expander';
 import { Experience } from './main-content/experience/experience';
+import { Education } from './main-content/education/education';
 
 @Component({
   selector: 'app-root',
-  imports: [MatSidenavModule, Heading, Expander, Experience],
+  imports: [MatSidenavModule, Heading, Expander, Experience, Education],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
