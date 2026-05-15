c1f889b3adc30bb034fc64a9d83848cf7c74ccec
Łukasz Solik <lukasz.s100@wp.pl>
2026-05-15 14:26:28 +0200

    CREATE: skill and courses components

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,12 @@ import { Heading } from './main-content/heading/heading';
 import { Expander } from './main-content/expander/expander';
 import { Experience } from './main-content/experience/experience';
 import { Education } from './main-content/education/education';
+import { Skills } from './drawer-content/skills/skills';
+import { Courses } from './drawer-content/courses/courses';
 
 @Component({
   selector: 'app-root',
-  imports: [MatSidenavModule, Heading, Expander, Experience, Education],
+  imports: [MatSidenavModule, Heading, Expander, Experience, Education, Skills, Courses],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
