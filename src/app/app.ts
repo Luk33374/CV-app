18500bf04e3f893781b65c096bc08e546280779a
Łukasz Solik <lukasz.s100@wp.pl>
2026-05-20 14:19:13 +0200

    Add expander button

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal, WritableSignal } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { Heading } from './main-content/heading/heading';
 import { Expander } from './main-content/expander/expander';
@@ -6,13 +6,29 @@ import { Experience } from './main-content/experience/experience';
 import { Education } from './main-content/education/education';
 import { Skills } from './drawer-content/skills/skills';
 import { Courses } from './drawer-content/courses/courses';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [MatSidenavModule, Heading, Expander, Experience, Education, Skills, Courses],
+  imports: [
+    MatSidenavModule,
+    MatButtonModule,
+    Heading,
+    Expander,
+    Experience,
+    Education,
+    Skills,
+    Courses,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
+  protected expanderOpened: WritableSignal<boolean> = signal(true);
+  protected buttonText = computed(() => (this.expanderOpened() ? 'Close' : 'Open'));
   protected readonly title = signal('CV-app');
+
+  protected toggleExpander() {
+    this.expanderOpened.set(!this.expanderOpened());
+  }
 }
